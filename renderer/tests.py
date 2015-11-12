8bdcbdbaf88357232f962c59021c769501532596
rouk1 <matt@dvmobile.fr>
2015-11-12 21:09:33 +0100

    test admin pages

--- renderer/tests.py
+++ renderer/tests.py
@@ -2,18 +2,25 @@
 # -*- coding: UTF-8 -*-
 from StringIO import StringIO
 
-import os
 from PIL import Image
-from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
 from models import MasterImage
 
 
 class RendererTest(TestCase):
+    def create_superuser(self):
+        username = 'admin'
+        password = '123456'
+
+        User.objects.create_superuser(username, 'a@a.fr', password)
+        return username, password
+
     def create_image(self):
         image_file = StringIO()
-        image = Image.new('RGBA', size=(50,50), color=(256,0,0))
+        image = Image.new('RGBA', size=(50, 50), color=(256, 0, 0))
         image.save(image_file, 'png')
         image_file.seek(0)
         master_image = MasterImage(
@@ -36,3 +43,25 @@ class RendererTest(TestCase):
         # why should I do that ?
         master_image.master.open()
         master_image.get_rendition_url(40, 40)
+
+    def test_admin_pages(self):
+        username, password = self.create_superuser()
+
+        c = Client()
+        c.login(username=username, password=password)
+
+        r = c.get(reverse('admin:renderer_masterimage_changelist'))
+        self.assertEqual(r.status_code, 200, (
+            'cant access changelist'
+        ))
+
+        r = c.get(reverse('admin:renderer_masterimage_add'))
+        self.assertEqual(r.status_code, 200, (
+            'cant access add page'
+        ))
+
+        master_image = self.create_image()
+        r = c.get(reverse('admin:renderer_masterimage_change', args=(master_image.pk, )))
+        self.assertEqual(r.status_code, 200, (
+            'cant access change page'
+        ))
