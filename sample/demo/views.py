fcf68d489197ef1f214bdc2c61b9e94a62912c11
rouk1 <matt@dvmobile.fr>
2015-11-15 20:53:13 +0100

    demo site scaffolding

--- sample/demo/views.py
+++ sample/demo/views.py
@@ -6,13 +6,39 @@ from renderer.models import MasterImage
 
 def index(request):
     m = MasterImage.objects.first()
-    renditions = [(0, 0)]
-    for i in range(0, 8):
+    return render(request, 'demo/index.html', {
+        'master': m,
+    })
+
+
+def aspect_ration_renditions(request):
+    m = MasterImage.objects.first()
+    renditions = []
+    for i in range(0, 4):
         renditions.append((
-            randint(0, m.master_width),
+            randint(0, min(m.master_width, 768/2)),
+            0,
+        ))
+    for i in range(0, 4):
+        renditions.append((
+            0,
             randint(0, m.master_height),
         ))
-    return render(request, 'demo/index.html', {
+    return render(request, 'demo/ratio.html', {
+        'master': m,
+        'renditions': renditions,
+    })
+
+
+def random_renditions(request):
+    m = MasterImage.objects.first()
+    renditions = []
+    for i in range(0, 8):
+        renditions.append((
+            randint(0, min(m.master_width, 768/2)),
+            randint(0, m.master_height),
+        ))
+    return render(request, 'demo/random.html', {
         'master': m,
         'renditions': renditions,
     })
