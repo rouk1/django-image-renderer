cba1ab202e243ff889764241ceb337d718f08bd4
rouk1 <matt@dvmobile.fr>
2015-11-15 23:58:59 +0100

    no media serving outside of dev server

--- sample/sample/urls.py
+++ sample/sample/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
     url(r'^', include('demo.urls', namespace='demo')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^renderer/', include('renderer.urls', namespace='renderer')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.AWS_SECRET_ACCESS_KEY is not None:
+    urlpatterns = urlpatterns + static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
 
 admin.site.site_header = 'Image Renderer Sample'
