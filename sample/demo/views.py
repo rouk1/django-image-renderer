cdcad1464dfbe70f2404b66efed0729d7642ef25
rouk1 <matt@dvmobile.fr>
2015-11-15 22:06:18 +0100

    sample project index page

--- sample/demo/views.py
+++ sample/demo/views.py
@@ -1,13 +1,22 @@
-from random import randint
+import markdown2
+import codecs
+import os
 
+from random import randint
+from django.conf import settings
 from django.shortcuts import render
 from renderer.models import MasterImage
 
 
 def index(request):
     m = MasterImage.objects.first()
+    readme_path = os.path.join(settings.BASE_DIR, '..', 'README.md')
+    input_file = codecs.open(readme_path, mode='r', encoding='utf-8')
+    text = input_file.read()
+    text = text.split('## Sample project', 1)[0]
+    html = markdown2.markdown(text, extras=['fenced-code-blocks', ])
     return render(request, 'demo/index.html', {
-        'master': m,
+        'text': html,
     })
 
 
